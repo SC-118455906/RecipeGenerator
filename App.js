3804b8bc834e143fa757df9df298eed19e4150dd
SC-118455906 <118455906@umail.ucc.ie>
2021-11-11 11:16:03 +0000

    inital commit

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Ingredient Store</Text>
+        <Text> test</Text>
+      <Image source={{uri: '../../assets/favicon.png'}} style={{ width: 200, height: 200 }}/>
       <StatusBar style="auto" />
     </View>
   );
